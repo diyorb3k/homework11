f5496f232d3ba9394ea8cdee9da98bad4a1fc3ac
unknown <you@example.com>
2024-05-14 08:29:55 +0600

    oufgyu

---
+++ main.js
@@ -0,0 +1,54 @@
+// 1 - Funksiya
+
+// {
+//   let b = prompt("");
+//   function Sal(str) {
+//     str = str.split("");
+//     let vowels = "aeiou";
+//     let sum = 0;
+//     for (let char of str) {
+//       if (vowels.includes(char)) sum++;
+//     }
+//     return sum;
+//   }
+//   console.log(Sal(b));
+// }
+
+// 2-funksiya
+// const arr = [];
+
+// function Ifn(a) {
+//   for (i = a; i > 0; i--) {
+//     arr.push(i);
+//   }
+//   return arr;
+// }
+// console.log(Ifn(5));
+// 6-funksiya
+// function sitafor(color) {
+//   switch (color) {
+//     case "green":
+//       return "yellow";
+//     case "yellow":
+//       return "red";
+//     case "red":
+//       return "green";
+//     default:
+//     //   return "Invalid color";
+//   }
+// }
+
+// console.log(sitafor("green"));
+// console.log(sitafor("yellow"));
+// console.log(sitafor("red"));
+// console.log(sitafor("blue"));
+
+// 7-functiond
+
+// function reverseString(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// console.log(reverseString("salom"));
+// console.log(reverseString("hello"));
+// console.log(reverseString("Imtihon"));
